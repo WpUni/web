14c7812f6556380e7a63f615c8c2e413f1edc89c
Rohit Suratekar <rohitsuratekar@gmail.com>
2020-04-15 19:43:30 +0530

    Working code

---
+++ src/views/Certificate.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="fill-height">
+        <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="8" lg="6">
+                <v-card class="pa-2">
+                    <v-card class="pa-2" outlined color="#ffefd6">
+                        <v-card class="pa-2" color="#fff9ef" outlined>
+                            <v-card-text>
+                                <p class="text-center font-weight-light">Certificate</p>
+                                <p class="text-center embossed old-font">University of WhatsApp</p>
+                                <p class="text-center font-italic">Upon recommendation from the admin committee <br> has
+                                    conferred on</p>
+                                <p class="text-center display-1 font-weight-black"> {{name}} </p>
+                                <p class="text-center">the degree of</p><br>
+                                <p class="text-center old-font embossed">Doctor of Philosophy</p><br>
+                                <p class="text-justify font-italic font-weight-medium">in recognition of her/his
+                                    attainment
+                                    in
+                                    special studies in science,
+                                    research and economics. His/her contribution in spreading unverified news and
+                                    misinformation
+                                    through various social media channels deserves a big applause.
+                                </p>
+                                <v-row align="center" justify="center">
+                                    <v-img src="../assets/seal.png" max-width="100px"/>
+                                </v-row>
+                                <v-row align="end" justify="center">
+                                    <v-col align="center">
+                                        <v-img src="../assets/sign1.png" max-height="100px" max-width="100px"/>
+                                        <p class="mt-1">Director, Uni. of WhatsApp</p>
+                                    </v-col>
+
+                                    <v-col align="center">
+                                        <v-img src="../assets/sign2.png" max-height="100px" max-width="100px"/>
+                                        <p class="mt-1">Registrar, Uni. of WhatsApp</p>
+                                    </v-col>
+                                </v-row>
+                            </v-card-text>
+                        </v-card>
+                    </v-card>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-footer>
+            <v-row justify="center" align="center">
+                <v-col cols="12">
+                    <p class="text-center"> Make your own certificate
+                        <router-link to="/">Here</router-link>
+                        and send it to the WhatsApp graduates.
+                    </p>
+                </v-col>
+            </v-row>
+
+
+        </v-footer>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {
+                name: "User Name"
+            }
+        },
+        created() {
+            let path = this.$router.currentRoute.path;
+            path = path.replace(/[^a-zA-Z0-9]/g, ' ');
+            this.name = toTitleCase(path);
+        }
+
+    }
+
+    function toTitleCase(str) {
+        return str.replace(
+            /\w\S*/g,
+            function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+    }
+</script>
+
+<style scoped>
+    @font-face {
+        font-family: "oldschool";
+        src: url("~@/assets/ManuskriptGothischUNZ1A.ttf");
+        font-style: normal;
+    }
+
+    .old-font {
+        font-family: oldschool, serif !important;
+        font-size: 2.125rem;
+        line-height: 1;
+        letter-spacing: 0.0073529412em;
+        font-weight: 400;
+    }
+
+    .embossed {
+        text-shadow: -1px -1px 1px #fff, 1px 1px 1px #000;
+        opacity: 0.8;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col cols="11" sm="8" md="8" lg="6">
+                <p class="text-center old-font">University of WhatsApp</p>
+            </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+            <v-col cols="11" sm="8" md="8" lg="6">
+                <p class="text-justify">
+                    With recent internet and social media age, fake news and misinformation are spreading at an alarming
+                    rate. In such a scenario, many people are confidently (and unintentionally) spreading
+                    misinformation.
+                    Many times this has led to panic and dangerous side-effects. There is a new term for such people in
+                    the Urban Dictionary. They are called <b>WhatsApp Graduates</b> from WhatsApp University
+                    <sup> <a href="https://www.urbandictionary.com/define.php?term=Whatsapp%20University"
+                             target="_blank">1</a></sup>.
+                    This websites salutes their efforts and provides a certificate. Please enter the name of the person
+                    in
+                    following the box and click on the 'Generate' button.
+                </p>
+            </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+            <v-col cols="11" sm="8" md="8" lg="6">
+                <v-text-field
+                        label="Person Name"
+                        v-model="name"
+                >
+                    <template v-slot:append>
+                        <v-btn
+                                depressed
+                                tile
+                                color="primary"
+                                @click="issue">
+                            Generate
+                        </v-btn>
+                    </template>
+                </v-text-field>
+            </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+            <v-col cols="11" sm="8" md="8" lg="6">
+                <p class="font-italic grey--text"> Tip: You can directly add person name to the url and generate the
+                    certificate. e.g. https://wpuni.online/your-name</p>
+            </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+            <v-col cols="11" sm="8" md="8" lg="6">
+                <v-card outlined color="#eceef1">
+                    <v-card-text class="text-justify">
+                        Note : This website is generated purely for fun. No discrimination,
+                        hate or ill-wish is intended. You should use this website ONLY if you agree to our
+                        <router-link to="terms">terms and
+                            conditions
+                        </router-link>
+                        . You can reach us at <a href="mailto:contact@wpuni.online">contact@wpuni.online</a>.
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "Home",
+        data() {
+            return {
+                name: ""
+            }
+        },
+        methods: {
+            issue: function () {
+                let temp = this.name.replace(/\s+/g, '-').toLowerCase();
+                this.$router.push({path: temp})
+            }
+        }
+    }
 </script>
+
+<style scoped>
+    @font-face {
+        font-family: "oldschool";
+        src: url("~@/assets/ManuskriptGothischUNZ1A.ttf");
+        font-style: normal;
+    }
+
+    .old-font {
+        font-family: oldschool, serif !important;
+        font-size: 2.125rem;
+        line-height: 1;
+        letter-spacing: 0.0073529412em;
+        font-weight: 400;
+    }
+</style>
